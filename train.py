e3e6e63f0907612d67bb48f3d07535258688cf07
sehandev <sehandev@gmail.com>
2022-02-10 16:12:56 +0900

    FEAT: swa

--- train.py
+++ train.py
@@ -3,7 +3,11 @@
 # PIP
 import torch
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from pytorch_lightning.callbacks import (
+    EarlyStopping,
+    ModelCheckpoint,
+    StochasticWeightAveraging,
+)
 from pytorch_lightning.loggers import WandbLogger
 
 # Custom
@@ -18,26 +22,27 @@ def train(cfg):
     )
 
     callback_list = []
-
     if cfg.callbacks.checkpoint:
         checkpoint_callback = ModelCheckpoint(
-            monitor='val_loss',
-            dirpath='./checkout/',
-            filename=cfg.logger.project + '-{epoch:04d}-{val_loss:.5f}',
+            monitor="val_loss",
+            dirpath="./checkout/",
+            filename=cfg.logger.project + "-{epoch:04d}-{val_loss:.5f}",
             save_top_k=1,
-            mode='min',
+            mode="min",
         )
         callback_list.append(checkpoint_callback)
-
     if cfg.callbacks.early_stop:
         early_stop_callback = EarlyStopping(
-            monitor='val_loss',
+            monitor="val_loss",
             min_delta=0.00,
             patience=cfg.callbacks.patience,
             verbose=False,
-            mode='min'
+            mode="min",
         )
         callback_list.append(early_stop_callback)
+    if cfg.callbacks.swa:
+        swa = StochasticWeightAveraging()
+        callback_list.append(swa)
 
     trainer = Trainer(
         logger=wandb_logger,
@@ -45,16 +50,17 @@ def train(cfg):
         gpus=cfg.trainer.gpus,
         enable_progress_bar=cfg.trainer.progress_bar,
         max_epochs=cfg.trainer.max_epochs,
-        strategy=cfg.trainer.strategy,  # dp, ddp, deepspeed_stage_3
+        strategy=cfg.trainer.strategy,
         precision=cfg.trainer.precision,
         deterministic=cfg.trainer.deterministic,
+        accumulate_grad_batches=2,
     )
 
     data_module = CustomDataModule(cfg)
     module = CustomModule(cfg)
 
-    print('Start model fitting')
+    print("Start model fitting")
     trainer.fit(module, datamodule=data_module)
 
-    print('Start testing')
-    trainer.test(datamodule=data_module, ckpt_path='best')
+    print("Start testing")
+    trainer.test(datamodule=data_module, ckpt_path="best")
