bc7386674bfe8f77d2a10087a44e808ee7cbbf1f
sehandev <sehandev@gmail.com>
2022-02-16 15:59:11 +0900

    EDIT IFNet

--- custom/softsplat/if_net.py
+++ custom/softsplat/if_net.py
@@ -8,18 +8,12 @@ class IFBlock(nn.Module):
     def __init__(self, in_planes, c=64):
         super().__init__()
         self.conv0 = nn.Sequential(
-            nn.Conv2d(in_planes, c // 2, 1, bias=False),
+            nn.Conv2d(in_planes, c // 2, 3, 2, 2, 2, bias=False),
             # nn.GroupNorm(c // 16, c // 2),
-            nn.ReLU(),
-            nn.Conv2d(c // 2, c // 2, 3, 2, 2, 2, bias=False),
-            # nn.GroupNorm(c // 16, c // 2),
-            nn.ReLU(),
-            nn.Conv2d(c // 2, c, 1, bias=False),
+            nn.PReLU(c // 2),
+            nn.Conv2d(c // 2, c, 3, 2, 2, 2, bias=False),
             # nn.GroupNorm(c // 16, c),
-            nn.ReLU(),
-            nn.Conv2d(c, c, 3, 2, 2, 2, bias=False),
-            # nn.GroupNorm(c // 16, c),
-            nn.ReLU(),
+            nn.PReLU(c),
         )
         self.convblock0 = nn.Sequential(
             self.conv(c, c, 3, 2, 2),
@@ -33,29 +27,15 @@ class IFBlock(nn.Module):
             self.conv(c, c, 3, 2, 2),
             self.conv(c, c, 3, 2, 2),
         )
-        self.flow_conv = nn.Sequential(
-            nn.ConvTranspose2d(c, c // 2, 1, bias=False),
-            # nn.GroupNorm(c // 16, c // 2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(c // 2, c // 2, 4, 2, 1, bias=False),
-            # nn.GroupNorm(c // 16, c // 2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(c // 2, 4, 1, bias=False),
-            # nn.GroupNorm(1, 4),
-            nn.ReLU(),
-            nn.ConvTranspose2d(4, 4, 4, 2, 1, bias=False),
+        self.convblock3 = nn.Sequential(
+            self.conv(c, c, 3, 2, 2),
+            self.conv(c, c, 3, 2, 2),
         )
-        self.mask_conv = nn.Sequential(
-            nn.ConvTranspose2d(c, c // 2, 1, bias=False),
+        self.flow_conv = nn.Sequential(
+            nn.ConvTranspose2d(c, c // 2, 4, 2, 1, bias=False),
             # nn.GroupNorm(c // 16, c // 2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(c // 2, c // 2, 4, 2, 1, bias=False),
-            # nn.GroupNorm(c // 16, c // 2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(c // 2, 1, 1, bias=False),
-            # nn.GroupNorm(1, 1),
-            nn.ReLU(),
-            nn.ConvTranspose2d(1, 1, 4, 2, 1, bias=False),
+            nn.PReLU(c // 2),
+            nn.ConvTranspose2d(c // 2, 4, 4, 2, 1, bias=False),
         )
 
     @staticmethod
@@ -64,21 +44,13 @@ class IFBlock(nn.Module):
             nn.Conv2d(
                 in_planes,
                 out_planes,
-                kernel_size=1,
-                bias=False,
-            ),
-            # nn.GroupNorm(out_planes // 16, out_planes),
-            nn.ReLU(),
-            nn.Conv2d(
-                out_planes,
-                out_planes,
                 kernel_size=kernel_size,
                 padding=padding,
                 dilation=dilation,
                 bias=False,
             ),
             # nn.GroupNorm(out_planes // 16, out_planes),
-            nn.ReLU(),
+            nn.PReLU(out_planes),
         )
 
     def forward(self, x, flow, scale):
@@ -90,25 +62,27 @@ class IFBlock(nn.Module):
                 align_corners=False,
                 recompute_scale_factor=False,
             )
-            flow = (
-                F.interpolate(
-                    flow,
-                    scale_factor=1.0 / scale,
-                    mode="bilinear",
-                    align_corners=False,
-                    recompute_scale_factor=False,
+            if flow is not None:
+                flow = (
+                    F.interpolate(
+                        flow,
+                        scale_factor=1.0 / scale,
+                        mode="bilinear",
+                        align_corners=False,
+                        recompute_scale_factor=False,
+                    )
+                    / scale
                 )
-                / scale
-            )
-        feature = torch.cat((x, flow), 1)
+        if flow is not None:
+            x = torch.cat((x, flow), 1)
 
-        feature = self.conv0(feature)
+        feature = self.conv0(x)
         feature = self.convblock0(feature) + feature
         feature = self.convblock1(feature) + feature
         feature = self.convblock2(feature) + feature
+        feature = self.convblock3(feature) + feature
 
         flow = self.flow_conv(feature)
-        mask = self.mask_conv(feature)
         flow = (
             F.interpolate(
                 flow,
@@ -119,96 +93,42 @@ class IFBlock(nn.Module):
             )
             * scale
         )
-        mask = F.interpolate(
-            mask,
-            scale_factor=scale,
-            mode="bilinear",
-            align_corners=False,
-            recompute_scale_factor=False,
-        )
 
-        return flow, mask
+        return flow
 
 
 class IFNet(nn.Module):
-    grid_cache = {}
-
     def __init__(self):
         super().__init__()
-        self.block0 = IFBlock(3 + 3 + 1 + 4, c=128)
-        self.block1 = IFBlock(3 + 3 + 1 + 4, c=128)
-        self.block2 = IFBlock(3 + 3 + 1 + 4, c=128)
-
-    def warp(self, input_tensor, flow):
-        key = flow.shape
-        if key in IFNet.grid_cache:
-            grid = IFNet.grid_cache[key]
-        else:
-            horizontal = torch.linspace(-1.0, 1.0, flow.shape[3])
-            horizontal = horizontal.view(1, 1, 1, flow.shape[3])
-            horizontal = horizontal.expand(flow.shape[0], -1, flow.shape[2], -1)
-            vertical = torch.linspace(-1.0, 1.0, flow.shape[2])
-            vertical = vertical.view(1, 1, flow.shape[2], 1)
-            vertical = vertical.expand(flow.shape[0], -1, -1, flow.shape[3])
-            grid = torch.cat([horizontal, vertical], 1)
-
-            IFNet.grid_cache[key] = grid
-        grid = grid.type_as(flow)
-
-        flow = torch.cat(
-            tensors=[
-                flow[:, 0:1, :, :] / ((input_tensor.shape[3] - 1.0) / 2.0),
-                flow[:, 1:2, :, :] / ((input_tensor.shape[2] - 1.0) / 2.0),
-            ],
-            dim=1,
-        )
-
-        grid = (grid + flow).permute(0, 2, 3, 1)
-        grid = F.grid_sample(
-            input=input_tensor,
-            grid=grid,
-            mode="bilinear",
-            padding_mode="border",
-            align_corners=True,
-        )
-        return grid
+        self.block0 = IFBlock(3 + 3 + 4, c=90)
+        self.block1 = IFBlock(3 + 3 + 4, c=90)
+        self.block2 = IFBlock(3 + 3 + 4, c=90)
 
     def forward(self, img1, img2, scale_list=[4, 2, 1]):
-        # img1, img2: (num_batches, 3, height, width)
-        warped_img1 = img1
-        warped_img2 = img2
+        # img1, img2: (batch_size, 3, height, width)
 
         [batch_size, channel, height, width] = img1.size()
 
         flow = torch.zeros([batch_size, 4, height, width]).type_as(img1)
-        mask = torch.zeros([batch_size, 1, height, width]).type_as(img1)
-        # flow: (num_batches, 4, height, width)
-        # mask: (num_batches, 1, height, width)
+        # flow: (batch_size, 4, height, width)
 
         block = [self.block0, self.block1, self.block2]
 
         for i in range(len(block)):
-            flow0, mask0 = block[i](
-                x=torch.cat((warped_img1, warped_img2, mask), 1),
+            new_flow1 = block[i](
+                x=torch.cat((img1, img2), 1),
                 flow=flow,
                 scale=scale_list[i],
             )
-            flow1, mask1 = block[i](
-                x=torch.cat((warped_img2, warped_img1, -mask), 1),
-                flow=torch.cat((flow[:, 2:4], flow[:, :2]), 1),
+            new_flow2 = block[i](
+                x=torch.cat((img2, img1), 1),
+                flow=torch.cat((flow[:, 2:], flow[:, :2]), 1),
                 scale=scale_list[i],
             )
-            flow1 = torch.cat((flow1[:, 2:4], flow1[:, :2]), 1)
+            new_flow = (new_flow1 + new_flow2) / 2
+            flow = flow + new_flow
 
-            # flow
-            flow = flow + (flow0 + flow1) / 2
+        flow_1to2 = flow[:, :2]
+        flow_2to1 = flow[:, 2:]
 
-            # mask
-            mask = mask + (mask0 + (-mask1)) / 2
-            mask = F.hardsigmoid(mask)
-
-            # warp
-            warped_img1 = self.warp(img1, flow[:, :2]) * mask
-            warped_img2 = self.warp(img2, flow[:, 2:4]) * (1 - mask)
-
-        return flow
+        return flow_1to2, flow_2to1
